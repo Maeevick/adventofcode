7433ed5198e5f6229818467ed85bcc208c2b00f8
Maeevick <maeevick@gmail.com>
2024-12-16 23:29:36 +0100

    chore: follow python naming conventions

--- with_python/src/main.py
+++ with_python/src/main.py
@@ -1,14 +1,14 @@
 from hello import hello
 from day1 import part1 as day1part1, part2 as day1part2
-from readInput import readInput
+from read_input import read_input
 
 
 def main():
     print("----------------")
     print(f"{hello()}")
     print("----------------")
-    print(f"D1P1: {day1part1(readInput("day1part1"))}")
-    print(f"D1P2: {day1part2(readInput("day1part1"))}")
+    print(f"D1P1: {day1part1(read_input("day1part1"))}")
+    print(f"D1P2: {day1part2(read_input("day1part1"))}")
     print("----------------")
 
 
--- with_python/tests/test_day1.py
+++ with_python/tests/test_day1.py
@@ -1,10 +1,10 @@
 from src.day1 import part1, part2
-from src.readInput import readInput
+from src.read_input import read_input
 
 
 def test_day1_part1_says():
-    assert part1(readInput("day1part1sample")) == 11
+    assert part1(read_input("day1part1sample")) == 11
 
 
 def test_day1_part2_says():
-    assert part2(readInput("day1part1sample")) == 31
+    assert part2(read_input("day1part1sample")) == 31
