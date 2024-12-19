35f48e6579596af1c994fd2bcd2f1bea3ca89c19
Maeevick <maeevick@gmail.com>
2024-12-19 17:07:07 +0100

    feat: solve day3 with_python

--- with_python/src/day3.py
+++ with_python/src/day3.py
@@ -1,6 +1,22 @@
+import re
+
 def part1(content: list[str]) -> int:
-    return 0
+    matches = re.finditer(r'mul\((\d{1,3}),(\d{1,3})\)', ''.join(content))
+    return sum(int(a) * int(b) for match in matches for a, b in [match.groups()])
 
 
 def part2(content: list[str]) -> int:
-    return 0
+    matches = re.finditer(r"(don't|do)\(\)|mul\((\d{1,3}),(\d{1,3})\)", ''.join(content))
+    processing = True
+    sum = 0
+    
+    for match in matches:
+        instruction, a, b = match.group(1), match.group(2), match.group(3)
+        
+        if instruction in ("do", "don't"):
+            processing = instruction == "do"
+        
+        if processing and a and b:
+            sum += int(a) * int(b)
+            
+    return sum
--- with_python/tests/test_day3.py
+++ with_python/tests/test_day3.py
@@ -2,8 +2,8 @@ from src.day3 import part1, part2
 from src.read_input import read_input
 
 def test_day3_part1_says():
-    assert part1(read_input("day3sample")) == 161
+    assert part1(read_input("day3part1sample")) == 161
 
 
 def test_day3_part2_says():
-    assert part2(read_input("day3sample")) == -1
+    assert part2(read_input("day3part2sample")) == 48
