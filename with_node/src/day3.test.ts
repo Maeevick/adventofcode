0811d49f7a2b52f6575bf9060fe3a412ac2f3629
Maeevick <maeevick@gmail.com>
2024-12-19 16:58:43 +0100

    feat: solve day3 with_node

--- with_node/src/day3.test.ts
+++ with_node/src/day3.test.ts
@@ -4,12 +4,12 @@ import test from "node:test";
 import { readInput } from "./readInput.ts";
 import { part1, part2 } from "./day3.ts";
 
-test("day2...", async (t) => {
+test("day3...", async (t) => {
   await t.test("part1 says...", () => {
-    assert.strictEqual(part1(readInput("day3sample")), 161);
+    assert.strictEqual(part1(readInput("day3part1sample")), 161);
   });
 
   await t.test("part2 says...", () => {
-    assert.strictEqual(part2(readInput("day3sample")), -1);
+    assert.strictEqual(part2(readInput("day3part2sample")), 48);
   });
 });
--- with_node/src/day3.ts
+++ with_node/src/day3.ts
@@ -1,7 +1,27 @@
 export function part1(content: string[]) {
-  return 0
+  const matches = content.join("").matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g);
+
+  return [...matches].reduce(
+    (sum, [_, a, b]) => sum + Number(a) * Number(b),
+    0
+  );
 }
 
 export function part2(content: string[]) {
-  return 0
+  const matches = content
+    .join("")
+    .matchAll(/(don't|do)\(\)|mul\((\d{1,3}),(\d{1,3})\)/g);
+
+  return [...matches].reduce(
+    (acc, [_, instruction, a, b]) => {
+      if (instruction) {
+        return { sum: acc.sum, processing: instruction === "do" };
+      }
+      if(acc.processing && a && b) {
+        return { sum: acc.sum + Number(a) * Number(b), processing: true }
+      }
+      return acc
+    },
+    { processing: true, sum: 0 }
+  ).sum;
 }
