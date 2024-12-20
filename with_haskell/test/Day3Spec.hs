6bf9179f3da33c2564d38791a73ded5e0d8e967e
Maeevick <maeevick@gmail.com>
2024-12-20 21:57:22 +0100

    feat: solve day2 with_haskell

--- with_haskell/test/Day3Spec.hs
+++ with_haskell/test/Day3Spec.hs
@@ -8,9 +8,9 @@ spec :: Spec
 spec = do
   describe "part 1 with sample data" $ do
     it "returns 161 (2*4 + 5*5 + 11*8 + 8*5)" $ do
-      content <- readInput "day3sample"
+      content <- readInput "day3part1sample"
       part1 content `shouldBe` 161
   describe "part2 with sample data" $ do
-    it "returns 4" $ do
-      content <- readInput "day2sample"
-      part2 content `shouldBe` -1
+    it "returns 48 (2*4 + 8*5)" $ do
+      content <- readInput "day3part2sample"
+      part2 content `shouldBe` 48
