6a2a513d845cb168b759b68810b10e2987194b05
Maeevick <maeevick@gmail.com>
2025-12-03 00:07:33 +0100

    feat: day2 part2

--- 2025/with_rust/src/day2.rs
+++ 2025/with_rust/src/day2.rs
@@ -1,3 +1,5 @@
+use fancy_regex::Regex;
+
 pub fn part1(content: Vec<String>) -> u64 {
     content[0]
         .split(',')
@@ -18,6 +20,56 @@ pub fn part1(content: Vec<String>) -> u64 {
         .sum()
 }
 
+// ~4200ms -> fancy but slow :D
+pub fn _part2_re(content: Vec<String>) -> u64 {
+    let re_with_any_pattern_repeated_more_than_once_composing_the_whole_string =
+        Regex::new(r"^(.+)\1+$").unwrap();
+    content[0]
+        .split(',')
+        .filter_map(|range| {
+            let parts: Vec<&str> = range.split('-').collect();
+            let start = parts[0].parse::<u64>().ok()?;
+            let end = parts[1].parse::<u64>().ok()?;
+            Some(
+                (start..=end)
+                    .filter(|&num| {
+                        re_with_any_pattern_repeated_more_than_once_composing_the_whole_string
+                            .is_match(&num.to_string())
+                            .expect("Doesn't match the fancy regexp with backtracking")
+                    })
+                    .sum::<u64>(),
+            )
+        })
+        .sum()
+}
+
+// ~900ms -> just slow ;)
+pub fn part2(content: Vec<String>) -> u64 {
+    /// Check if repeatable pattern equals original number representation
+    /// ## Arguments
+    /// - s: &str: the number representation
+    /// ## Examples:
+    /// - s = "123123", len = 3: "123".repeat(6/3) = "123123" ✅
+    /// - s = "123456", len = 3: "123".repeat(6/2) = "123123" != "123456" ❌
+    fn is_repeating_pattern(s: &str) -> bool {
+        (1..=s.len() / 2).any(|len| s == s[0..len].repeat(s.len() / len))
+    }
+
+    content[0]
+        .split(',')
+        .filter_map(|range| {
+            let parts: Vec<&str> = range.split('-').collect();
+            let start = parts[0].parse::<u64>().ok()?;
+            let end = parts[1].parse::<u64>().ok()?;
+            Some(
+                (start..=end)
+                    .filter(|&num| is_repeating_pattern(&num.to_string()))
+                    .sum::<u64>(),
+            )
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,5 +78,11 @@ mod tests {
     #[test]
     fn part1solution() {
         assert_eq!(part1(reader::read_input("day2sample")), 1227775554);
+    }
+
+    #[test]
+    fn part2solution() {
+        assert_eq!(_part2_re(reader::read_input("day2sample")), 4174379265);
+        assert_eq!(part2(reader::read_input("day2sample")), 4174379265);
     }
 }
--- 2025/with_rust/src/main.rs
+++ 2025/with_rust/src/main.rs
@@ -3,17 +3,29 @@ mod day2;
 mod hello;
 mod reader;
 
+use std::time::Instant;
+
 fn main() {
     println!("----------------");
     let hello = hello::hello();
     println!("\n{hello}\n\n");
     println!("----------------");
+
+    let start = Instant::now();
     let day1part1 = day1::part1(reader::read_input("day1").clone());
-    println!("D1P1: {day1part1}");
+    println!("D1P1: {day1part1} ({}ms)", start.elapsed().as_millis());
+
+    let start = Instant::now();
     let day1part2 = day1::part2(reader::read_input("day1").clone());
-    println!("D1P2: {day1part2}");
+    println!("D1P2: {day1part2} ({}ms)", start.elapsed().as_millis());
     println!("----------------");
+
+    let start = Instant::now();
     let day2part1 = day2::part1(reader::read_input("day2").clone());
-    println!("D2P1: {day2part1}");
+    println!("D2P1: {day2part1} ({}ms)", start.elapsed().as_millis());
+
+    let start = Instant::now();
+    let day2part2 = day2::part2(reader::read_input("day2").clone());
+    println!("D2P2: {day2part2} ({}ms)", start.elapsed().as_millis());
     println!("----------------");
 }
