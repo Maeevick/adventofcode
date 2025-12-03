a820a0fce9ab9ef2c3dfc32d3cd3a89c2b9146b4
Maeevick <maeevick@gmail.com>
2025-12-04 00:27:52 +0100

    feat: day3 part1

--- 2025/with_rust/src/day3.rs
+++ 2025/with_rust/src/day3.rs
@@ -1,5 +1,33 @@
-pub fn part1(content: Vec<String>) -> u64 {
-    42
+pub fn part1(content: Vec<String>) -> u32 {
+    content
+        .iter()
+        .map(|line| {
+            let chars: Vec<char> = line.chars().collect();
+
+            let (fst, fst_idx) = line.chars().enumerate().take(chars.len() - 1).fold(
+                (0, 0),
+                |(fst, idx), (pos, c)| {
+                    let digit = c.to_digit(10).unwrap();
+                    if digit > fst {
+                        (digit, pos)
+                    } else {
+                        (fst, idx)
+                    }
+                },
+            );
+
+            let snd = line
+                .chars()
+                .enumerate()
+                .skip(fst_idx + 1)
+                .fold(0, |snd, (_, c)| {
+                    let digit = c.to_digit(10).unwrap();
+                    snd.max(digit)
+                });
+
+            fst * 10 + snd
+        })
+        .sum()
 }
 
 pub fn part2(content: Vec<String>) -> u64 {
@@ -13,7 +41,7 @@ mod tests {
 
     #[test]
     fn part1solution() {
-        assert_eq!(part1(reader::read_input("day3sample")), 666);
+        assert_eq!(part1(reader::read_input("day3sample")), 357);
     }
 
     #[test]
