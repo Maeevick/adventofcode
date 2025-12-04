d6f0153bd0068c0f5b4b2a09fad4a131cd6c7f19
Maeevick <maeevick@gmail.com>
2025-12-04 23:22:30 +0100

    feat: day4 part2

--- 2025/with_rust/src/day4.rs
+++ 2025/with_rust/src/day4.rs
@@ -1,25 +1,5 @@
 pub fn part1(content: Vec<String>) -> usize {
-    fn count_adjacent_rolls(grid: &[Vec<char>], row: usize, col: usize) -> usize {
-        [
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-            (0, -1),
-            (0, 1),
-            (1, -1),
-            (1, 0),
-            (1, 1),
-        ]
-        .iter()
-        .filter(|(dr, dc)| {
-            grid.get((row as isize + dr) as usize)
-                .and_then(|r| r.get((col as isize + dc) as usize))
-                .map_or(false, |&cell| cell == '@')
-        })
-        .count()
-    }
-
-    let grid: Vec<Vec<char>> = content.iter().map(|line| line.chars().collect()).collect();
+    let grid: Vec<Vec<char>> = generate_grid(content);
 
     grid.iter()
         .enumerate()
@@ -32,8 +12,48 @@ pub fn part1(content: Vec<String>) -> usize {
         .sum()
 }
 
-pub fn part2(content: Vec<String>) -> u64 {
-    42
+pub fn part2(content: Vec<String>) -> usize {
+    fn count_then_remove(mut grid: Vec<Vec<char>>) -> usize {
+        let accessible: Vec<_> = (0..grid.len())
+            .flat_map(|row| (0..grid[row].len()).map(move |col| (row, col)))
+            .filter(|&(row, col)| {
+                grid[row][col] == '@' && count_adjacent_rolls(&grid, row, col) < 4
+            })
+            .collect();
+        if accessible.is_empty() {
+            return 0;
+        }
+        for (row, col) in &accessible {
+            grid[*row][*col] = 'x';
+        }
+        accessible.len() + count_then_remove(grid)
+    }
+
+    count_then_remove(generate_grid(content))
+}
+
+fn generate_grid(content: Vec<String>) -> Vec<Vec<char>> {
+    content.iter().map(|line| line.chars().collect()).collect()
+}
+
+fn count_adjacent_rolls(grid: &[Vec<char>], row: usize, col: usize) -> usize {
+    [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ]
+    .iter()
+    .filter(|(dr, dc)| {
+        grid.get((row as isize + dr) as usize)
+            .and_then(|r| r.get((col as isize + dc) as usize))
+            .map_or(false, |&cell| cell == '@')
+    })
+    .count()
 }
 
 #[cfg(test)]
@@ -48,6 +68,6 @@ mod tests {
 
     #[test]
     fn part2solution() {
-        assert_eq!(part2(reader::read_input("day4sample")), 666);
+        assert_eq!(part2(reader::read_input("day4sample")), 43);
     }
 }
