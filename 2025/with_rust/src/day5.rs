ce3fc699a898450f09b93c1f524cbe7b78c99621
Maeevick <maeevick@gmail.com>
2025-12-05 16:41:11 +0100

    feat: day5 part1

--- 2025/with_rust/src/day5.rs
+++ 2025/with_rust/src/day5.rs
@@ -1,5 +1,17 @@
 pub fn part1(content: Vec<String>) -> usize {
-    42
+    let parts: Vec<_> = content.split(|line| line.is_empty()).collect();
+    parts[1]
+        .iter()
+        .map(|item| item.parse::<i64>().unwrap())
+        .filter(|&item| {
+            parts[0]
+                .iter()
+                .map(|range| range.split_once("-").unwrap())
+                .any(|(start, end)| {
+                    item >= start.parse::<i64>().unwrap() && item <= end.parse::<i64>().unwrap()
+                })
+        })
+        .count()
 }
 
 pub fn part2(content: Vec<String>) -> usize {
@@ -13,7 +25,7 @@ mod tests {
 
     #[test]
     fn part1solution() {
-        assert_eq!(part1(reader::read_input("day5sample")), 666);
+        assert_eq!(part1(reader::read_input("day5sample")), 3);
     }
 
     #[test]
